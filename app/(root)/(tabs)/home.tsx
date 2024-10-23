81d34b5719cbd70f15158d01c4bab9ccd65d87b1
Eric Noubissi <eric-colday.noubissi@deepomatic.com>
2024-10-23 17:53:43 +0200

    map, googleplaces, stores, youtube part zustand

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,10 +1,15 @@
+import GoogleTextInput from "@/components/GoogleTextInput";
+import Map from "@/components/Map";
 import RideCard from "@/components/RideCard";
 import { icons, images } from "@/constants";
+import { useLocationStore } from "@/store";
 import { useUser } from "@clerk/clerk-expo";
 import { useAuth } from "@clerk/clerk-expo";
 import { router } from "expo-router";
+import { useEffect, useState } from "react";
 import { ActivityIndicator, FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import * as Location from "expo-location";
 
 const recentRides = [
   {
@@ -111,9 +116,46 @@ const Home = () => {
   const { signOut } = useAuth();
   const loading = false;
 
+  const { setUserLocation, setDestinationLocation } = useLocationStore();
+
   const handleSignOut = () => {
     signOut();
     router.replace("/(auth)/sign-in");
+  };
+
+  const [hasPermission, setHasPermission] = useState<boolean>(false);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setHasPermission(false);
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+
+      const address = await Location.reverseGeocodeAsync({
+        latitude: location.coords?.latitude!,
+        longitude: location.coords?.longitude!,
+      });
+
+      setUserLocation({
+        latitude: location.coords?.latitude,
+        longitude: location.coords?.longitude,
+        address: `${address[0].name}, ${address[0].region}`,
+      });
+    })();
+  }, []);
+
+  const handleDestinationPress = (location: {
+    latitude: number;
+    longitude: number;
+    address: string;
+  }) => {
+    setDestinationLocation(location);
+
+    router.push("/(root)/find-ride");
   };
 
   return (
@@ -145,19 +187,37 @@ const Home = () => {
           </View>
         )}
         ListHeaderComponent={() => (
-          <View className="flex flex-row items-center justify-between my-5">
-            <Text className="text-2xl font-JakartaExtraBold">
-              {user?.firstName || user?.emailAddresses[0].emailAddress}👋
+          <>
+            <View className="flex flex-row items-center justify-between my-5">
+              <Text className="text-2xl font-JakartaExtraBold">
+                {user?.firstName || user?.emailAddresses[0].emailAddress}👋
+              </Text>
+              <TouchableOpacity
+                onPress={handleSignOut}
+                className="justify-center items-center w-10 h-10 rounded-full bg-white"
+              >
+                <Image source={icons.out} className="w-4 h-4" />
+              </TouchableOpacity>
+            </View>
+
+            <GoogleTextInput
+              icon={icons.search}
+              containerStyle="bg-white shadow-md shadow-neutral-300"
+              handlePress={handleDestinationPress}
+            />
+            <>
+              <Text className="text-xl font-JakartaBold mt-5 mb-3">
+                Your current location
+              </Text>
+              <View className="flex flex-row items-center bg-transparent h-[300px]">
+                <Map />
+              </View>
+            </>
+            <Text className="text-xl font-JakartaBold mt-5 mb-3">
+              Recent Rides
             </Text>
-            <TouchableOpacity
-              onPress={handleSignOut}
-              className="justify-center items-center w-10 h-10 rounded-full bg-white"
-            >
-              <Image source={icons.out} className="w-4 h-4" />
-            </TouchableOpacity>
-          </View>
-        )
-        }
+          </>
+        )}
       />
     </SafeAreaView>
   );
